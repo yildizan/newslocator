9bcedddcf137b4948dca793d4ef6095bf6140953
anilyildiz <anilyildiz59@gmail.com>
2022-03-02 00:34:01 +0300

    fixed style and param

--- src/main/java/com/yildizan/newsfrom/locator/client/DiscordClient.java
+++ src/main/java/com/yildizan/newsfrom/locator/client/DiscordClient.java
@@ -8,13 +8,13 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="discordClient", url = "${discord.webhook.url}", configuration = FeignConfiguration.class)
+@FeignClient(name = "discordClient", url = "${discord.webhook.url}", configuration = FeignConfiguration.class)
 public interface DiscordClient {
 
     @PostMapping(value = "${discord.webhook.params.info}", consumes = MediaType.APPLICATION_JSON_VALUE)
     void notifyInfo(@RequestBody InfoDto infoDto);
 
-    @PostMapping(value = "${discord.webhook.params.info}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "${discord.webhook.params.error}", consumes = MediaType.APPLICATION_JSON_VALUE)
     void notifyError(@RequestBody ErrorDto errorDto);
 
 }
