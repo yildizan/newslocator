7de0b7b1b92bfaa25f001de4795342009d04e85b
anilyildiz <anilyildiz59@gmail.com>
2022-03-03 23:43:19 +0300

    removed news phrase feature

--- src/main/java/com/yildizan/newsfrom/locator/repository/NewsPhraseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yildizan.newsfrom.locator.repository;
-
-import com.yildizan.newsfrom.locator.entity.BufferNewsPhrase;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NewsPhraseRepository extends CrudRepository<BufferNewsPhrase, Integer> {
-
-}
