b04580027ca3184309e6dfa20a56d2cb15914f42
yildizan <anil.yildiz@outlook.com.tr>
2026-03-25 14:36:08 +0100

    added retry mechanism for openai

--- src/main/java/com/yildizan/newsfrom/locator/configuration/OpenAiClientConfiguration.java
+++ src/main/java/com/yildizan/newsfrom/locator/configuration/OpenAiClientConfiguration.java
@@ -1,10 +1,14 @@
 package com.yildizan.newsfrom.locator.configuration;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import feign.Request;
 import feign.RequestInterceptor;
+import feign.Retryer;
 
 @Configuration
 public class OpenAiClientConfiguration {
@@ -15,6 +19,16 @@ public class OpenAiClientConfiguration {
     @Bean
     public RequestInterceptor openAiApiKeyInterceptor() {
         return requestTemplate -> requestTemplate.header("Authorization", "Bearer " + apiKey);
+    }
+
+    @Bean
+    public Request.Options openAiRequestOptions() {
+        return new Request.Options(5, TimeUnit.SECONDS, 120, TimeUnit.SECONDS, true);
+    }
+
+    @Bean
+    public Retryer openAiRetryer() {
+        return new Retryer.Default(1000, 3000, 3);
     }
 
 }
